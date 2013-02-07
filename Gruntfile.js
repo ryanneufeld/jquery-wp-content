dbb921b0f74a6ffe20750e7a84b72da143034e82
adam j. sontag <ajpiano@ajpiano.com>
2013-02-07 05:00:18 -0500

    Build process: Add stylus as a dependency and include it in Gruntfile. Move css/base.css into stylus/base.styl, and process it with Stylus.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,11 +36,32 @@ module.exports = function(grunt) {
 			dist: ["themes/jquery/dist"]
 		},
 
+		stylus: {
+			compile: {
+				/*
+				options: {
+					paths: ['path/to/import', 'another/to/import'],
+					urlfunc: 'embedurl',
+					// use embedurl('test.png') in our code to trigger Data URI embedding
+					use: [
+					require('fluidity') // use stylus plugin at compile time
+					]
+					import: [ //  @import 'foo', 'bar/moo', etc. into every .styl file
+					'foo', //  that is compiled. These might be findable based on values you gave
+					'bar/moo' //  to `paths`, or a plugin you added under `use`
+					]
+				},
+				*/
+				files: {
+					"themes/jquery/dist/output.css": ["themes/jquery/stylus/*.styl"] // compile and concat into single file
+				}
+			}
+		},
 
 		mincss: {
 			build: {
 				files: {
-					"themes/jquery/dist/index.css": "themes/jquery/css/base.css"
+					"themes/jquery/dist/index.css": "themes/jquery/dist/output.css"
 				}
 			}
 		},
@@ -61,11 +82,12 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-copy");
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-mincss");
+	grunt.loadNpmTasks("grunt-contrib-stylus");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-watch");
 
 	// By default, lint and run all tests.
-	grunt.registerTask("default", ["jshint", "clean", "uglify", "mincss"]);
+	grunt.registerTask("default", ["jshint", "clean", "uglify", "stylus", "mincss"]);
 
 };
 
